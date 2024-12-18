6498f32dbf68e74572e2038b16a6d9282296af9a
Adriel Gian <adrielgian99@gmail.com>
2024-12-19 01:25:24 +0700

    Update

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,10 +1,12 @@
 const { Firestore } = require('@google-cloud/firestore');
- 
+
 async function storeData(id, data) {
-  const db = new Firestore();
- 
-  const predictCollection = db.collection('prediction');
-  return predictCollection.doc(id).set(data);
+
+ const db = new Firestore({
+
+databaseId: 'predictions'
+
+})
 }
- 
+
 module.exports = storeData;
